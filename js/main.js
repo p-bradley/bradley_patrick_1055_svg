1c0575fa0332dac013969f54540489051bea28b3
p-bradley <p_bradley@fanshaweonline.ca>
2019-01-24 22:36:52 -0500

    added console log triggers

--- js/main.js
+++ js/main.js
@@ -1,7 +1,89 @@
-let theDrinkingAndDriving = document.querySelector("#drinkinganddriving");
+		let theDrinkingAndDriving = document.querySelector("#drinkinganddriving");
 
 		function logThisDrinkingAndDriving() {
 			console.log(this.id);
 		}
 
 		theDrinkingAndDriving.addEventListener("click", logThisDrinkingAndDriving);
+
+
+		let theAmbulance = document.querySelector("#ambulance");
+
+		function logThisAmbulance() {
+			console.log(this.id);
+		}
+
+		theAmbulance.addEventListener("click", logThisAmbulance);
+
+
+		let theCautionSign = document.querySelector("#cautionsign");
+
+		function logThisCautionSign() {
+			console.log(this.id);
+		}
+
+		theCautionSign.addEventListener("click", logThisCautionSign);
+
+
+		let theCar = document.querySelector("#car");
+
+		function logThisCar() {
+			console.log(this.id);
+		}
+
+		theCar.addEventListener("click", logThisCar);
+
+
+		let theKey = document.querySelector("#key");
+
+		function logThisKey() {
+			console.log(this.id);
+		}
+
+		theKey.addEventListener("click", logThisKey);
+
+
+		let theMarker = document.querySelector("#marker");
+
+		function logThisMarker() {
+			console.log(this.id);
+		}
+
+		theMarker.addEventListener("click", logThisMarker);
+
+
+		let theSeatBelt = document.querySelector("#seatbelt");
+
+		function logThisSeatBelt() {
+			console.log(this.id);
+		}
+
+		theSeatBelt.addEventListener("click", logThisSeatBelt);
+
+
+		let theStopLights = document.querySelector("#stoplights");
+
+		function logThisStopLights() {
+			console.log(this.id);
+		}
+
+		theStopLights.addEventListener("click", logThisStopLights);
+
+
+		let theStopSign = document.querySelector("#stopsign");
+
+		function logThisStopSign() {
+			console.log(this.id);
+		}
+
+		theStopSign.addEventListener("click", logThisStopSign);
+
+
+		let theGasPump = document.querySelector("#gaspump");
+
+		function logThisGasPump() {
+			console.log(this.id);
+		}
+
+		theGasPump.addEventListener("click", logThisGasPump);
+
