99d233aa7d9f58432b63345018f6d1bea351571e
p-bradley <p_bradley@fanshaweonline.ca>
2019-01-24 22:57:23 -0500

    added on triggers to javascript css

--- js/main.js
+++ js/main.js
@@ -87,3 +87,57 @@
 
 		theGasPump.addEventListener("click", logThisGasPump);
 
+document.querySelector("#stoplights").addEventListener('click', slfill);
+function slfill() {
+	document.getElementById('stoplights').classList.add("fill");
+}
+
+document.querySelector("#ambulance").addEventListener('click', afill);
+function afill() {
+	document.getElementById('ambulance').classList.add("fill");
+}
+
+document.querySelector("#car").addEventListener('click', cfill);
+function cfill() {
+	document.getElementById('car').classList.add("fill");
+}
+
+document.querySelector("#cautionsign").addEventListener('click', csfill);
+function csfill() {
+	document.getElementById('cautionsign').classList.add("fill");
+
+
+
+
+
+}
+
+document.querySelector("#drinkinganddriving").addEventListener('click', dfill);
+function dfill() {
+	document.getElementById('drinkinganddriving').classList.add("fill");
+}
+
+document.querySelector("#gaspump").addEventListener('click', gfill);
+function gfill() {
+	document.getElementById('gaspump').classList.add("fill");
+}
+
+document.querySelector("#key").addEventListener('click', kfill);
+function kfill() {
+	document.getElementById('key').classList.add("fill");
+}
+
+document.querySelector("#marker").addEventListener('click', mfill);
+function mfill() {
+	document.getElementById('marker').classList.add("fill");
+}
+
+document.querySelector("#seatbelt").addEventListener('click', sbfill);
+function sbfill() {
+	document.getElementById('seatbelt').classList.add("fill");
+}
+
+document.querySelector("#stopsign").addEventListener('click', ssfill);
+function ssfill() {
+	document.getElementById('stopsign').classList.add("fill");
+}
